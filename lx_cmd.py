89f4e62ad52feeba4cb2a38d5ecd0c5efaa7378a
Priyanshu-Hestabit <priyanshu.hestabit@gmail.com>
2023-10-18 16:28:44 +0530

    for better gpu use and smooth experience | added a defualt config file | if removed then pass some variables | it helped to overcome an unwanted error

--- lx_cmd.py
+++ lx_cmd.py
@@ -9,6 +9,7 @@ class Command:
 install_deps = Command(name="Dependies Installation",
                        cmd = "pip3 install datasets==2.14.5 transformers==4.34.0 accelerate==0.23.0 torchvision==0.16.0 ftfy==6.1.1 tensorboard==2.14.1",
                        help = "It helps to install the required dependies in the Environemnt")
+                       #torch version depends on hardware to utilize GPU
 
 diffuser_clone = Command(name = "Clone Diffuser",
                          cmd = "git clone https://github.com/huggingface/diffusers.git",
@@ -19,6 +20,6 @@ install_diffuser = Command(name = "Diffuser Installation",
                            help = "Install the diffuser library from diffuser_clone")
 
 set_acc_precision_fp16 = Command(name = "Accelerate Precision fp16",
-                                 cmd = "accelerate config",
+                                 cmd = "accelerate config default --mixed_precision fp16",
                                  help = "It helps to initialize the accelerate config file and put precision to fp16")
                                 #  cmd = "accelerate config default --mixed_precision fp16",
